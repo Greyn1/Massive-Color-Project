cbb56ad3b38f7559208683b21c315af06bf1a4f6
Greyn1 <dheryabengani@outlook.com>
2022-04-01 00:27:56 +0530

    added color-boxes in palette.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Palette from './Palette';
+import seedColors from './seedColors';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>App</h1>
+        <Palette {...seedColors[3]} />
       </div>
     );
   }
--- src/Palette.js
+++ src/Palette.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import ColorBox from './ColorBox';
+import './Palette.css';
 
 class Palette extends Component {
     render() {
+        const ColorBoxes = this.props.colors.map(color => (
+            <ColorBox key={color.name} background={color.color} name={color.name} />
+        ));
         return (
             <div className='palette' >
                 {/* navbar */}
                 <div className='palette-colors' >
-                    {/* bunch of color boxes */}
+                    {/* Bunch of colorboxes */}
+                    {ColorBoxes}    
                 </div>
                 {/* footer */}
             </div>
